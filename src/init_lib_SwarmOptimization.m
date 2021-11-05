a745897b8b61882f073291e5f08c0e46452dd0e7
Pietro Mosca <pietromosca1994@gmail.com>
2021-11-05 15:51:18 +0100

    added init_project file

---
+++ src/init_lib_SwarmOptimization.m
@@ -0,0 +1,9 @@
+%% Initialize project
+%% Author: Pietro Mosca
+%% Email: pietromosca1994@gmail.com
+%% Date: 19.02.2021
+
+%% Addpath
+
+[project_path,~,~] = fileparts(mfilename('fullpath'));
+addpath(genpath(project_path));
